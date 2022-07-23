5fb22d2e975a70ab458cb4c8a6f3b1823be34ad6
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2022-07-23 14:34:46 -0700

    Slideshow viewport fixed

---
+++ src/components/Homepage/Slideshow.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import styles from './Slideshow.module.css';
+const Slideshow = () => {
+  return <div className={styles.slideshow}></div>;
+};
+
+export default Slideshow;
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import Header from '../components/Header';
+import Header from '../components/Homepage/Header';
+import Slideshow from '../components/Homepage/Slideshow';
 import styles from './Homepage.module.css';
 
 const Homepage = () => {
-  return <Header />;
+  return (
+    <div className={styles.homepage}>
+      <div className={styles.top}>
+        <Header />
+        <Slideshow />
+      </div>
+      <div className={styles.bottom}>
+        <Header />
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
