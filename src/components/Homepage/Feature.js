44850f1bdbbf40763b9b347c20472520127fd78c
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2022-07-26 11:03:27 -0700

    Buttons in Feature Section

--- src/components/Homepage/Feature.js
+++ src/components/Homepage/Feature.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import TwoGrid from './TwoGrid';
 import FeatureSlides from './FeatureSlides';
 import FeatureBody from './FeatureBody';
@@ -18,17 +19,12 @@ const Feature = () => {
         />
         <FeatureBody
           title="Visual Display Boards"
-          description="Nelson Adams Naco is a leading company with competetitive pricing."
+          description="Nelson Adams Naco is a leading company with competitive pricing."
         >
           <div className={styles.dropdown_box}>
-            <div className={styles.dropdown_button}>Dropdown Here</div>
-            {/* <ul className={styles.dropdown_list}>
-              <li>HOME</li>
-              <li>Products</li>
-              <li>Architect Corner</li>
-              <li>About Us</li>
-              <li>Contact Us</li>
-            </ul> */}
+            <Link to="/products">
+              <div className={styles.dropdown_button}>Shop Products</div>
+            </Link>
           </div>
         </FeatureBody>
       </TwoGrid>
@@ -37,8 +33,16 @@ const Feature = () => {
       <TwoGrid>
         <FeatureBody
           title="Tables and Benches"
-          description="Our Tables and Benches are our leading products with competetitive pricing."
-        />
+          description="Our Tables and Benches are our leading products with competitive pricing."
+        >
+          <div className={styles.dropdown_box}>
+            <Link to="/tables">
+              <div className={styles.dropdown_button}>
+                Shop Tables and Benches
+              </div>
+            </Link>
+          </div>
+        </FeatureBody>
         <FeatureSlides
           image1="./images/tableAndBenches/tnb1.jpg"
           alt1="table1"
@@ -50,17 +54,23 @@ const Feature = () => {
       </TwoGrid>
       <TwoGrid>
         <FeatureSlides
-          image1="./images/products/blackboard.jpg"
+          image1="./images/vendors/forbo.jpg"
           alt1="blackboard"
-          image2="./images/products/markerboard.jpg"
+          image2="./images/vendors/guilfordmane.jpg"
           alt2="blackboard"
-          image3="./images/products/Tackboard.jpg"
+          image3="./images/vendors/koroseal.jpg"
           alt3="blackboard"
         />
         <FeatureBody
           title="Our Vendors"
-          description="Nelson Adams Naco is proud to be partners with Koroseal, Forbo, parlumber, Guilford mane."
-        />
+          description="Nelson Adams Naco is proud to be partners with Koroseal, Forbo, parlumber, and Guilford mane."
+        >
+          <div className={styles.dropdown_box}>
+            <Link to="/">
+              <div className={styles.dropdown_button}>Learn More</div>
+            </Link>
+          </div>
+        </FeatureBody>
       </TwoGrid>
     </section>
   );
