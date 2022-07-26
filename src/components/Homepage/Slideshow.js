1bdb7ce1db8bdb4f8b004f13c36b1cfc5742ba38
Oniel Gutierrez <gutierrez.onionie@gmail.com>
2022-07-26 01:00:09 -0700

    Changed Slideshow

--- src/components/Homepage/Slideshow.js
+++ src/components/Homepage/Slideshow.js
@@ -5,26 +5,33 @@ import styles from './Slideshow.module.css';
 const Slideshow = () => {
   return (
     <Carousel fade className={styles.slideshow}>
+      {/* Escribano */}
       <Carousel.Item className={styles.item}>
         <img src="./images/main_slideshow/escribano.jpg" alt="escribano-img" />
         <Carousel.Caption className={`${styles.caption} ${styles.caption1}`}>
-          <h1 className={styles.caption_title}>Escribano</h1>
-          <h3>A Nelson Adams Product</h3>
-          <h3>Reinvent education with modern technology</h3>
+          <div className={styles.caption_title}>Escribano</div>
+          <div className={styles.subtitle}>A Nelson Adams Naco Product</div>
+          <div className={styles.desc}>
+            Reinvent education with modern technology
+          </div>
           <Link to="/escribano">
             <button>Learn More</button>
           </Link>
         </Carousel.Caption>
       </Carousel.Item>
 
+      {/* Visual Display */}
       <Carousel.Item className={styles.item}>
         <img
           src="./images/main_slideshow/visualdisplay.jpg"
           alt="visual-board"
         />
         <Carousel.Caption className={`${styles.caption} ${styles.caption2}`}>
-          <h1 className={styles.caption_title}>Visual Display Unit</h1>
-          <h3>A Description About Visual Display Unit</h3>
+          <div className={styles.caption_title}>Visual Display Unit</div>
+
+          <div className={styles.desc}>
+            A description about Visual Display Unit
+          </div>
           <Link to="/visual">
             <button>Learn More</button>
           </Link>
@@ -38,8 +45,12 @@ const Slideshow = () => {
           alt="tables-and-benches"
         />
         <Carousel.Caption className={`${styles.caption} ${styles.caption3}`}>
-          <h1 className={styles.caption_title}>Tables and Benches</h1>
-          <h3>A Description About Tables and Benches</h3>
+          <div className={styles.caption_title}>Tables and Benches</div>
+
+          <div className={styles.desc}>
+            A Description About Tables and Benches
+          </div>
+
           <Link to="/tables">
             <button>Learn More</button>
           </Link>
@@ -50,8 +61,10 @@ const Slideshow = () => {
       <Carousel.Item className={styles.item}>
         <img src="./images/main_slideshow/main_4.jpg" alt="tackwall-panels" />
         <Carousel.Caption className={`${styles.caption} ${styles.caption4}`}>
-          <h1 className={styles.caption_title}>Tackwall Panels</h1>
-          <h3>A Description About Tackwall Panels</h3>
+          <div className={styles.caption_title}>Tackwall Panels</div>
+
+          <div className={styles.desc}>A Description About Tackwall Panels</div>
+
           <Link to="/tackwall">
             <button>Learn More</button>
           </Link>
@@ -65,8 +78,10 @@ const Slideshow = () => {
           alt="display-cases"
         />
         <Carousel.Caption className={`${styles.caption} ${styles.caption5}`}>
-          <h1 className={styles.caption_title}>Display Cases</h1>
-          <h3>A Description About Display Cases</h3>
+          <div className={styles.caption_title}>Display Cases</div>
+
+          <div className={styles.desc}>A Description About Display Cases</div>
+
           <Link to="/display">
             <button>Learn More</button>
           </Link>
