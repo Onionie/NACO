5585223b8f601c901a14bb2062de4ec4c1bdbb20
onionie <gutierrez.onionie@gmail.com>
2022-08-04 20:09:47 -0700

    Remove JQuery

--- src/components/ProductsPage/Slider.js
+++ src/components/ProductsPage/Slider.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import './Slider.css';
+import $ from 'jquery';
+
+// images
+import chalkboard from '../../assets/images/VisualBoards/chalkboard.jpg';
+import bulletinBoard from '../../assets/images/VisualBoards/enclosedBulletinBoard.jpg';
+import tackBoard from '../../assets/images/VisualBoards/forboBlueTackBoard.jpg';
 
 const Slider = () => {
-  return <section className="slider">Slider</section>;
+  $('.slide').hiSlide();
+  return (
+    <section className="slider">
+      <div className="slide hi-slide">
+        <div className="hi-prev"></div>
+        <div className="hi-next"></div>
+
+        <ul>
+          <li>
+            <img src={chalkboard} alt="chalkboard" />
+            <img src={bulletinBoard} alt="bulletin-board" />
+            <img src={tackBoard} alt="tackboard" />
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
 };
 
 export default Slider;
