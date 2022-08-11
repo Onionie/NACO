a09bce7116c64808ba75a627cdfc81873485ad69
onionie <gutierrez.onionie@gmail.com>
2022-08-11 00:00:31 -0700

    Markerboards: Description Body Done

--- src/pages/ProductsPage/VisualDisplays/Markerboard.js
+++ src/pages/ProductsPage/VisualDisplays/Markerboard.js
@@ -54,11 +54,34 @@ const Markerboard = () => {
               <li>
                 Porcelain enamel steel writing surface with aluminum trim.
               </li>
-              <li></li>
-              <li></li>
+              <li>
+                1/2" particleboard with optional .005 or .015 moisture barrier
+                backer.
+              </li>
+              <li>
+                Standard satin clear anodized aluminum that include 2ea: C-4 or
+                C-18 side trim, standard CR-4 blade tray with radius edges.
+              </li>
+              <li>
+                Optional 1" or 2" map-rail with various insert options: natural
+                cork, Forbo or vinyl covered natural cork insert at additional
+                cost.
+              </li>
+              <li>
+                All aluminum is 6063-T5 alloy that comes in satin standard clear
+                anodized color.
+              </li>
             </ul>
           </div>
         </div>
+      </div>
+
+      {/* Product Information */}
+      <div className={styles.product_specifications}>
+        <div className={styles.product_specifications_title}>
+          Product Information
+        </div>
+        <div></div>
       </div>
     </section>
   );
