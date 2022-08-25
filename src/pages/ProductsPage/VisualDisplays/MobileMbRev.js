28abae1132617bfcf0ff6df01e58916602da7533
onionie <gutierrez.onionie@gmail.com>
2022-08-25 15:55:22 -0700

    Reversible MobileMB: Render Images and Title

--- src/pages/ProductsPage/VisualDisplays/MobileMbRev.js
+++ src/pages/ProductsPage/VisualDisplays/MobileMbRev.js
@@ -1,7 +1,42 @@
 import React from 'react';
 
+import ProductSection from '../../../components/SpecificProduct/ProductSection';
+import HeroProduct from '../../../components/SpecificProduct/HeroProduct';
+import HeroBody from '../../../components/SpecificProduct/HeroBody';
+import ProductInfo from '../../../components/SpecificProduct/ProductInfo';
+import AvailableSizes from '../../../components/SpecificProduct/AvailableSizes';
+
+import reversible from '../../../assets/images/VisualBoards/MobileMarkerboard/reversible.jpg';
+import cbNmb from '../../../assets/images/VisualBoards/Finishes/cbNmb.jpg';
+
 const MobileMbRev = () => {
-  return <div>MobileMbRev</div>;
+  const mainImages = [
+    {
+      id: 1,
+      image: reversible,
+      alt: 'reversible_markerboard',
+    },
+  ];
+
+  const availImages = [
+    {
+      id: 1,
+      image: cbNmb,
+      alt: 'porcelain_finishes',
+    },
+  ];
+  return (
+    <ProductSection>
+      <HeroProduct mainImages={mainImages} indicators={false} controls={false}>
+        <HeroBody
+          title="Reversible Mobile Markerboard"
+          availImages={availImages}
+          indicators={false}
+          controls={false}
+        ></HeroBody>
+      </HeroProduct>
+    </ProductSection>
+  );
 };
 
 export default MobileMbRev;
