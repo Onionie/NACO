aac7b817b0d2ab50fca873bbcf047a2089340ca6
onionie <gutierrez.onionie@gmail.com>
2022-08-28 14:30:07 -0700

    EnclosedBB: Slide Images

--- src/pages/ProductsPage/VisualDisplays/EnclosedBB.js
+++ src/pages/ProductsPage/VisualDisplays/EnclosedBB.js
@@ -1,7 +1,33 @@
 import React from 'react';
 
+import ProductSection from '../../../components/SpecificProduct/ProductSection';
+import HeroProduct from '../../../components/SpecificProduct/HeroProduct';
+
+import eNC from '../../../assets/images/VisualBoards/EnclosedBB/enclosedBB_nc.jpg';
+import eFB from '../../../assets/images/VisualBoards/EnclosedBB/enclosedBB_f.jpg';
+
 const EnclosedBB = () => {
-  return <div>EnclosedBB</div>;
+  const mainImages = [
+    {
+      id: 1,
+      image: eNC,
+      alt: 'enclosedBB_nc',
+    },
+    {
+      id: 2,
+      image: eFB,
+      alt: 'enclosedBB_f',
+    },
+  ];
+  return (
+    <ProductSection>
+      <HeroProduct
+        mainImages={mainImages}
+        indicators={true}
+        controls={true}
+      ></HeroProduct>
+    </ProductSection>
+  );
 };
 
 export default EnclosedBB;
