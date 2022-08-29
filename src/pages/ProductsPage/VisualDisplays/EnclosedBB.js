37f83e0a58fc4e43028a082ee1a1fb7c195ff94e
onionie <gutierrez.onionie@gmail.com>
2022-08-29 00:25:02 -0700

    EnclosedBB: Available Sizes

--- src/pages/ProductsPage/VisualDisplays/EnclosedBB.js
+++ src/pages/ProductsPage/VisualDisplays/EnclosedBB.js
@@ -3,6 +3,8 @@ import React from 'react';
 import ProductSection from '../../../components/SpecificProduct/ProductSection';
 import HeroProduct from '../../../components/SpecificProduct/HeroProduct';
 import HeroBody from '../../../components/SpecificProduct/HeroBody';
+import ProductInfo from '../../../components/SpecificProduct/ProductInfo';
+import AvailableSizes from '../../../components/SpecificProduct/AvailableSizes';
 
 import eNC from '../../../assets/images/VisualBoards/EnclosedBB/enclosedBB_nc.jpg';
 import eFB from '../../../assets/images/VisualBoards/EnclosedBB/enclosedBB_f.jpg';
@@ -86,6 +88,24 @@ const EnclosedBB = () => {
           </li>
         </HeroBody>
       </HeroProduct>
+      <ProductInfo>
+        <AvailableSizes>
+          <li>4' x 4'</li>
+          <li>4' x 5'</li>
+          <li>4' x 6'</li>
+          <li>4' x 7'</li>
+          <li>4' x 8'</li>
+          <li>4' x 9'</li>
+          <li>4' x 10'</li>
+          <li>4' x 11'</li>
+          <li>4' x 12'</li>
+          <li>4' x 13'</li>
+          <li>4' x 14'</li>
+          <li>4' x 15'</li>
+          <li>4' x 16'</li>
+          <li>Custom Sizes Available</li>
+        </AvailableSizes>
+      </ProductInfo>
     </ProductSection>
   );
 };
