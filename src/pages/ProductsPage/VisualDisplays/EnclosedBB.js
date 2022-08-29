a5eb43f3e9bc4c90e555b4d146e61af607cb87a1
onionie <gutierrez.onionie@gmail.com>
2022-08-29 00:19:58 -0700

    EnclosedBB: Available Colors

--- src/pages/ProductsPage/VisualDisplays/EnclosedBB.js
+++ src/pages/ProductsPage/VisualDisplays/EnclosedBB.js
@@ -6,6 +6,13 @@ import HeroBody from '../../../components/SpecificProduct/HeroBody';
 
 import eNC from '../../../assets/images/VisualBoards/EnclosedBB/enclosedBB_nc.jpg';
 import eFB from '../../../assets/images/VisualBoards/EnclosedBB/enclosedBB_f.jpg';
+import forboColors from '../../../assets/images/VisualBoards/Finishes/forbo_colors_group.jpg';
+import bel from '../../../assets/images/VisualBoards/Finishes/BEL1.png';
+import dessand1 from '../../../assets/images/VisualBoards/Finishes/DesertSand1.png';
+import dessand2 from '../../../assets/images/VisualBoards/Finishes/DesertSand2.png';
+import mojave1 from '../../../assets/images/VisualBoards/Finishes/Mojave1.png';
+import mojave2 from '../../../assets/images/VisualBoards/Finishes/Mojave2.png';
+import harborW from '../../../assets/images/VisualBoards/Finishes/HarborWeave_group.png';
 
 const EnclosedBB = () => {
   const mainImages = [
@@ -20,14 +27,53 @@ const EnclosedBB = () => {
       alt: 'enclosedBB_f',
     },
   ];
+
+  const availImages = [
+    {
+      id: 0,
+      image: forboColors,
+      alt: 'forbo_colors',
+    },
+    {
+      id: 1,
+      image: bel,
+      alt: 'belair_colors',
+    },
+    {
+      id: 2,
+      image: dessand1,
+      alt: 'desert_sand_colors',
+    },
+    {
+      id: 3,
+      image: dessand2,
+      alt: 'desert_sand_colors',
+    },
+    {
+      id: 4,
+      image: mojave1,
+      alt: 'mojave_colors',
+    },
+    {
+      id: 5,
+      image: mojave2,
+      alt: 'mojave_colors',
+    },
+    {
+      id: 6,
+      image: harborW,
+      alt: 'harbor_weave_colors',
+    },
+  ];
+
   return (
     <ProductSection>
       <HeroProduct mainImages={mainImages} indicators={true} controls={true}>
         <HeroBody
           title="Enclosed Bulletin Board"
-          availImages={mainImages}
-          indicators={false}
-          controls={false}
+          availImages={availImages}
+          indicators={true}
+          controls={true}
         >
           <li>Enclosed Bulletin Board Cabinets feature aluminum trim. </li>
           <li>Comes with locking hinged doors and tempered clear glass.</li>
