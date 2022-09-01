e1d2cab3cc9c68770b51f6bc80bcbeef7cc5ff75
onionie <gutierrez.onionie@gmail.com>
2022-08-31 22:00:10 -0700

    Homepage: Cleanup Images

--- src/components/Homepage/Slideshow.js
+++ src/components/Homepage/Slideshow.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import escribanoWalk from '../../assets/images/homepage/escribano_walk1.jpg';
 import escribano from '../../assets/images/homepage/escribano.png';
