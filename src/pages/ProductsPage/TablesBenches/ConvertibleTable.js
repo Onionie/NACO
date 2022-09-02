b08b5b0be0f96dfeeaed8b66a444cf5f8521f936
onionie <gutierrez.onionie@gmail.com>
2022-09-01 17:21:06 -0700

    ConvertibleTable: Product Slide Images

--- src/pages/ProductsPage/TablesBenches/ConvertibleTable.js
+++ src/pages/ProductsPage/TablesBenches/ConvertibleTable.js
@@ -1,7 +1,41 @@
 import React from 'react';
 
+import ProductSection from '../../../components/SpecificProduct/ProductSection';
+import HeroProduct from '../../../components/SpecificProduct/HeroProduct';
+import HeroBody from '../../../components/SpecificProduct/HeroBody';
+
+import ctb1 from '../../../assets/images/TablesAndBenches/ConvertibleTables/ctb_1.png';
+import ctb2 from '../../../assets/images/TablesAndBenches/ConvertibleTables/ctb_2.png';
+import ctb3 from '../../../assets/images/TablesAndBenches/ConvertibleTables/ctb_3.png';
+
 const ConvertibleTable = () => {
-  return <div>ConvertibleTable</div>;
+  const mainImages = [
+    {
+      id: 1,
+      image: ctb1,
+      alt: 'convertible_table',
+    },
+    {
+      id: 2,
+      image: ctb2,
+      alt: 'convertible_table',
+    },
+    {
+      id: 3,
+      image: ctb3,
+      alt: 'convertible_table',
+    },
+  ];
+
+  return (
+    <ProductSection>
+      <HeroProduct
+        mainImages={mainImages}
+        indicators={true}
+        controls={true}
+      ></HeroProduct>
+    </ProductSection>
+  );
 };
 
 export default ConvertibleTable;
