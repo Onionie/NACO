fa1a20bf076e1fb8046194768c5bbfe27c5a7afa
onionie <gutierrez.onionie@gmail.com>
2022-09-02 01:38:03 -0700

    RoundTables: Product Image Slider

--- src/pages/ProductsPage/TablesBenches/RoundTable.js
+++ src/pages/ProductsPage/TablesBenches/RoundTable.js
@@ -1,7 +1,30 @@
 import React from 'react';
 
+import ProductSection from '../../../components/SpecificProduct/ProductSection';
+import HeroProduct from '../../../components/SpecificProduct/HeroProduct';
+import HeroBody from '../../../components/SpecificProduct/HeroBody';
+import ProductInfo from '../../../components/SpecificProduct/ProductInfo';
+
+import rtb1 from '../../../assets/images/TablesAndBenches/RoundTables/rtb_1.png';
+
 const RoundTable = () => {
-  return <div>RoundTable</div>;
+  const mainImages = [
+    {
+      id: 1,
+      image: rtb1,
+      alt: 'round_table',
+    },
+  ];
+
+  return (
+    <ProductSection>
+      <HeroProduct
+        mainImages={mainImages}
+        indicators={false}
+        controls={false}
+      ></HeroProduct>
+    </ProductSection>
+  );
 };
 
 export default RoundTable;
