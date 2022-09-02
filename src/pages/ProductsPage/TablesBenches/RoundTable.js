c33e1cf188845e140ecf2ecd9855429b27221d6c
onionie <gutierrez.onionie@gmail.com>
2022-09-02 01:52:25 -0700

    RoundTables: Available Finishes

--- src/pages/ProductsPage/TablesBenches/RoundTable.js
+++ src/pages/ProductsPage/TablesBenches/RoundTable.js
@@ -7,6 +7,8 @@ import ProductInfo from '../../../components/SpecificProduct/ProductInfo';
 
 import rtb1 from '../../../assets/images/TablesAndBenches/RoundTables/rtb_1.png';
 
+import ctb_finishes from '../../../assets/images/TablesAndBenches/ctb_finishes.png';
+
 const RoundTable = () => {
   const mainImages = [
     {
@@ -16,13 +18,37 @@ const RoundTable = () => {
     },
   ];
 
+  const availImages = [
+    {
+      id: 1,
+      image: ctb_finishes,
+      alt: 'table_color_options',
+    },
+  ];
+
   return (
     <ProductSection>
-      <HeroProduct
-        mainImages={mainImages}
-        indicators={false}
-        controls={false}
-      ></HeroProduct>
+      <HeroProduct mainImages={mainImages} indicators={false} controls={false}>
+        <HeroBody
+          title="Round Tables"
+          availImages={availImages}
+          indicators={false}
+          controls={false}
+        >
+          <li>
+            Choose an option from the standard bench, individual stool seating,
+            or a combination of bench and stool.
+          </li>
+          <li>
+            Two wheel chair accessible models are available that provides access
+            for two wheelchair users along with seating for six.
+          </li>
+          <li>
+            When moving or storing, the storage latch keeps the tables secured
+            in the upright position.{' '}
+          </li>
+        </HeroBody>
+      </HeroProduct>
     </ProductSection>
   );
 };
