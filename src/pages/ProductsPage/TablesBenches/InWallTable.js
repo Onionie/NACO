7676709ab28f082947b3b9b282acdaf947e44752
onionie <gutierrez.onionie@gmail.com>
2022-09-04 00:38:24 -0700

    InWallTable: Product Slide Image

--- src/pages/ProductsPage/TablesBenches/InWallTable.js
+++ src/pages/ProductsPage/TablesBenches/InWallTable.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import styles from './InWallTable.module.css';
+
+import ProductSection from '../../../components/SpecificProduct/ProductSection';
+import HeroProduct from '../../../components/SpecificProduct/HeroProduct';
+import HeroBody from '../../../components/SpecificProduct/HeroBody';
+import ProductInfo from '../../../components/SpecificProduct/ProductInfo';
+import TableComponent from '../../../components/SpecificProduct/TablesBenches/TableComponent';
+
+import iwtb1 from '../../../assets/images/TablesAndBenches/InWallTables/iwtb_1.png';
 
 const InWallTable = () => {
-  return <div>InWallTable</div>;
+  const mainImages = [
+    {
+      id: 1,
+      image: iwtb1,
+      alt: 'in-wall_table',
+    },
+  ];
+  return (
+    <ProductSection>
+      <HeroProduct
+        mainImages={mainImages}
+        indicators={false}
+        controls={false}
+      ></HeroProduct>
+    </ProductSection>
+  );
 };
 
 export default InWallTable;
