704a8cf4ba3ed540aa32728ed505286ba6612f47
onionie <gutierrez.onionie@gmail.com>
2022-09-09 04:01:09 -0700

    TWP: Product Images

--- src/pages/ProductsPage/TackwallSystems/TackwallPanel.js
+++ src/pages/ProductsPage/TackwallSystems/TackwallPanel.js
@@ -6,10 +6,37 @@ import HeroProduct from '../../../components/SpecificProduct/HeroProduct';
 import HeroBody from '../../../components/SpecificProduct/HeroBody';
 import ProductInfo from '../../../components/SpecificProduct/ProductInfo';
 
-import fdw_1 from '../../../assets/images/TackwallSystems/FDW/fdw_1.png';
+import twp_1 from '../../../assets/images/TackwallSystems/TWP/twp_1.png';
+import twp_2 from '../../../assets/images/TackwallSystems/TWP/twp_2.png';
+import twp_3 from '../../../assets/images/TackwallSystems/TWP/twp_3.png';
 
 const TackwallPanel = () => {
-  return <div>TackwallPanel</div>;
+  const mainImages = [
+    {
+      id: 1,
+      image: twp_1,
+      alt: 'tackwall_panel',
+    },
+    {
+      id: 2,
+      image: twp_2,
+      alt: 'tackwall_panel',
+    },
+    {
+      id: 3,
+      image: twp_3,
+      alt: 'tackwall_panel',
+    },
+  ];
+  return (
+    <ProductSection>
+      <HeroProduct
+        mainImages={mainImages}
+        indicators={true}
+        controls={true}
+      ></HeroProduct>
+    </ProductSection>
+  );
 };
 
 export default TackwallPanel;
