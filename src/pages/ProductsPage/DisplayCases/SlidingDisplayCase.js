8bb965c54c59ab64fdd8ae26929c4a794eca2650
onionie <gutierrez.onionie@gmail.com>
2022-09-10 22:12:58 -0700

    SlidingDisplayCase: Available Colors

--- src/pages/ProductsPage/DisplayCases/SlidingDisplayCase.js
+++ src/pages/ProductsPage/DisplayCases/SlidingDisplayCase.js
@@ -9,6 +9,14 @@ import DetailsAndLight from '../../../components/SpecificProduct/DisplayCases/De
 import sdc from '../../../assets/images/DisplayCases/SlidingDisplayCase/sdc_main.png';
 import sdc_detail from '../../../assets/images/DisplayCases/SlidingDisplayCase/sdc_det.png';
 
+import forboColors from '../../../assets/images/VisualBoards/Finishes/forbo_colors_group.jpg';
+import bel from '../../../assets/images/VisualBoards/Finishes/BEL1.png';
+import dessand1 from '../../../assets/images/VisualBoards/Finishes/DesertSand1.png';
+import dessand2 from '../../../assets/images/VisualBoards/Finishes/DesertSand2.png';
+import mojave1 from '../../../assets/images/VisualBoards/Finishes/Mojave1.png';
+import mojave2 from '../../../assets/images/VisualBoards/Finishes/Mojave2.png';
+import harborW from '../../../assets/images/VisualBoards/Finishes/HarborWeave_group.png';
+
 const SlidingDisplayCase = () => {
   const mainImages = [
     {
@@ -17,14 +25,52 @@ const SlidingDisplayCase = () => {
       alt: 'sliding_display_case',
     },
   ];
+
+  const availImages = [
+    {
+      id: 0,
+      image: forboColors,
+      alt: 'forbo_colors',
+    },
+    {
+      id: 1,
+      image: bel,
+      alt: 'belair_colors',
+    },
+    {
+      id: 2,
+      image: dessand1,
+      alt: 'desert_sand_colors',
+    },
+    {
+      id: 3,
+      image: dessand2,
+      alt: 'desert_sand_colors',
+    },
+    {
+      id: 4,
+      image: mojave1,
+      alt: 'mojave_colors',
+    },
+    {
+      id: 5,
+      image: mojave2,
+      alt: 'mojave_colors',
+    },
+    {
+      id: 6,
+      image: harborW,
+      alt: 'harbor_weave_colors',
+    },
+  ];
   return (
     <ProductSection>
       <HeroProduct mainImages={mainImages} indicators={false} controls={false}>
         <HeroBody
           title="Sliding Display Case"
-          availImages={mainImages}
-          indicators={false}
-          controls={false}
+          availImages={availImages}
+          indicators={true}
+          controls={true}
         >
           <li>Consists of 2ea standard shelves.</li>
           <li>Standard sliding doors with optional fixed back panel.</li>
