5aa8ccc3290ab118792938dfc3536e0ca6a2bbd0
onionie <gutierrez.onionie@gmail.com>
2022-09-12 23:32:54 -0700

    Escribano: Video Working

--- src/pages/EscribanoPage.js
+++ src/pages/EscribanoPage.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import './EscribanoPage.css';
+
+import ReactPlayer from 'react-player/youtube';
 
 const EscribanoPage = () => {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        marginTop: '15rem',
-        fontSize: '4rem',
-      }}
-    >
-      Escribano Page
-    </div>
+    <section className="section_escribano">
+      <div className="player-wrapper">
+        <ReactPlayer
+          url="https://www.youtube.com/watch?v=ne5OEKFoLm8"
+          className="react-player"
+          playing
+          width="80%"
+          height="80%"
+          controls={true}
+          volume={1}
+          muted={true}
+        />
+      </div>
+    </section>
   );
 };
 
