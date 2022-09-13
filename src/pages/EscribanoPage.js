03f72b81e4eb7e490096d45152b8cf84b10b1f9e
onionie <gutierrez.onionie@gmail.com>
2022-09-13 04:02:50 -0700

    Escribano: Video Section

--- src/pages/EscribanoPage.js
+++ src/pages/EscribanoPage.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import './EscribanoPage.css';
+import styles from './EscribanoPage.module.css';
 
 import ReactPlayer from 'react-player/youtube';
 
 const EscribanoPage = () => {
   return (
-    <section className="section_escribano">
-      <div className="player-wrapper">
+    <section className={styles.section_escribano}>
+      <div className={styles.header_box}>
+        <div className={styles.header_title}>Escribano</div>
+        <div className={styles.header_subtitle}>
+          A fun and interactive board that is evolving the world of Education.
+        </div>
+      </div>
+      <div className={styles.player_wrapper}>
         <ReactPlayer
           url="https://www.youtube.com/watch?v=ne5OEKFoLm8"
-          className="react-player"
+          className={styles.react_player}
           playing
-          width="80%"
-          height="80%"
+          width="60%"
+          height="60%"
           controls={true}
           volume={1}
           muted={true}
