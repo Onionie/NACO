fd7c7668e3e2a258faa321a9779ec034ac9eac7f
onionie <gutierrez.onionie@gmail.com>
2022-09-17 23:26:10 -0700

    MarkerWall: Product Info and Details

--- src/pages/ProductsPage/WallSystems/MarkerWall.js
+++ src/pages/ProductsPage/WallSystems/MarkerWall.js
@@ -10,6 +10,8 @@ import mw_1 from '../../../assets/images/TackwallSystems/MarkerWall/mw_1.png';
 import mw_2 from '../../../assets/images/TackwallSystems/MarkerWall/mw_2.png';
 import mw_3 from '../../../assets/images/TackwallSystems/MarkerWall/mw_3.png';
 import mw_4 from '../../../assets/images/TackwallSystems/MarkerWall/mw_4.png';
+import mw_colors from '../../../assets/images/TackwallSystems/MarkerWall/mw_colors.PNG';
+import mw_details from '../../../assets/images/TackwallSystems/MarkerWall/mw_details.png';
 
 const MarkerWall = () => {
   const mainImages = [
@@ -34,13 +36,50 @@ const MarkerWall = () => {
       alt: 'marker_wall',
     },
   ];
+
+  const availImages = [
+    {
+      id: 1,
+      image: mw_colors,
+      alt: 'availbale_colors',
+    },
+  ];
+
   return (
     <ProductSection>
-      <HeroProduct
-        mainImages={mainImages}
-        indicators={true}
-        controls={true}
-      ></HeroProduct>
+      <HeroProduct mainImages={mainImages} indicators={true} controls={true}>
+        <HeroBody
+          title="Marker Wall"
+          availImages={availImages}
+          indicators={false}
+          controls={false}
+        >
+          <li>
+            Made up of polyvision porcelain panels in standard 4'0" increments.
+          </li>
+          <li>
+            Cover a wall completely, from floor to ceiling, with up to 16'0" of
+            height.
+          </li>
+          <li>Concealed Joints.</li>
+          <li>Highly Durable.</li>
+          <li>Easy to clean.</li>
+          <li>Colorfast will not fade.</li>
+          <li>Scratch, bacteria, chemical, and fire resistant.</li>
+          <li>No made-to-order components.</li>
+          <li>Smooth, inert surface.</li>
+          <li>Sophisticated aesthetic.</li>
+          <li>Framed and frameless options available.</li>
+          <li>Vertical and horizontal options.</li>
+        </HeroBody>
+      </HeroProduct>
+      <ProductInfo>
+        <div className={styles.mw_details}>
+          <div>
+            <img src={mw_details} alt="marker_wall_details" />
+          </div>
+        </div>
+      </ProductInfo>
     </ProductSection>
   );
 };
