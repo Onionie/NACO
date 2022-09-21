15c6f592fda12e37b75deb6013e19699b8766f0c
onionie <gutierrez.onionie@gmail.com>
2022-09-21 03:27:46 -0700

    AutoCAD: Working hrefs

--- src/pages/ArchitectPage/AutoCAD.js
+++ src/pages/ArchitectPage/AutoCAD.js
@@ -1,10 +1,46 @@
 import React from 'react';
 import styles from './AutoCAD.module.css';
 
+import { libraries, tnb } from './links';
+
 const ArchitectCornerPage = () => {
   return (
     <div className={styles.architects_corner}>
       <div className={styles.architects_title}>AutoCAD Library</div>
+
+      <div className={styles.ac_grid}>
+        {/* Trims */}
+        <div>
+          <div className={styles.ac_title}>Trims, Sliders and More</div>
+          <ul className={styles.library_list}>
+            {libraries.map((library) => (
+              <li key={library.id}>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href={library.link}
+                >
+                  <b>{library.series}</b> {library.product}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        {/* TnB */}
+        <div>
+          <div className={styles.ac_title}>Tables and benches</div>
+          <ul className={styles.library_list}>
+            {tnb.map((tb) => (
+              <li key={tb.id}>
+                <a target="_blank" rel="noopener noreferrer" href={tb.link}>
+                  <b>{tb.series}</b> {tb.product}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/pages/ArchitectPage/links.js
@@ -0,0 +1,133 @@
+const libraries = [
+  {
+    id: 1,
+    series: '1000 SERIES',
+    product: 'Blade Tray C-4 Trim',
+    link: 'https://drive.google.com/file/d/1V8TnbM5m-BwfaLsR_gXl9vAXCh0xpMw0/view',
+  },
+  {
+    id: 2,
+    series: '1500 SERIES',
+    product: 'Blade Tray C-18 Trim',
+    link: 'https://drive.google.com/file/d/1Rh7PrXvw7p5ovWRTcA3AVffFDgfC8oID/view',
+  },
+  {
+    id: 3,
+    series: '2000 SERIES',
+    product: 'Box Tray C-4 Trim',
+    link: 'https://drive.google.com/file/d/1xVBYM68pHaUDEri3jJhY-MARSKsnqcMt/view',
+  },
+  {
+    id: 4,
+    series: '2500 SERIES',
+    product: 'Box Tray C-18 Trim',
+    link: 'https://drive.google.com/file/d/1m8DqGsOXSLsgJ-EeIFF8aqVRWEG-YUlt/view',
+  },
+  {
+    id: 5,
+    series: 'FHS SERIES',
+    product: 'Full Height Slider Series',
+    link: 'https://drive.google.com/file/d/1GQ6SQLu5KnJv-gwuk7FXaytriTeX4rq_/view',
+  },
+  {
+    id: 6,
+    series: 'HSS SERIES',
+    product: 'Horizontal Surface Mount Slider',
+    link: 'https://drive.google.com/file/d/1zO8cAHqqAWc8wGS2Xsq4g8weAD7LZtN_/view',
+  },
+  {
+    id: 7,
+    series: 'TWS SERIES',
+    product: 'Teaching Wall Sliders',
+    link: 'https://drive.google.com/file/d/1lQ8JOw5iGvry5Wr4Kt9fwhvD5sngMFo_/view',
+  },
+  {
+    id: 8,
+    series: 'VFM SERIES',
+    product: 'Vertical Floor Mounted',
+    link: 'https://drive.google.com/file/d/10VOfCwZcGFi22tmAa68WSEtB-Xw5yJKJ/view',
+  },
+  {
+    id: 9,
+    series: 'VWM SERIES',
+    product: 'Vertical Wall Mounted',
+    link: 'https://drive.google.com/file/d/13ZjvMX1r5tJ08vzvEEUcXmdQiR1OeFMO/view',
+  },
+  {
+    id: 10,
+    series: 'FDW SERIES',
+    product: 'Forbo Directly To Wall',
+    link: 'https://drive.google.com/file/d/1WshQ7m2pzACf4dtIBPFRuMty3s995Awv/view',
+  },
+  {
+    id: 11,
+    series: 'TWP SERIES',
+    product: 'Tackwall Panels',
+    link: 'https://drive.google.com/file/d/1KdbY6g4H5c7NwHzI2-qT0GH2woawRY_0/view?usp=drive_open',
+  },
+  {
+    id: 12,
+    series: 'HDC SERIES',
+    product: 'Hinged Display Cases',
+    link: 'https://drive.google.com/file/d/1KG7R2JseWmZIDCHNpld8HSzDhSMH8Ncc/view',
+  },
+  {
+    id: 13,
+    series: 'SDC SERIES',
+    product: 'Sliding Display Cases',
+    link: 'https://drive.google.com/file/d/1j2zr1NwJ_8gP84-wvZBS-a1Pt29ZRCG9/view',
+  },
+];
+
+const tnb = [
+  {
+    id: 1,
+    series: 'DRM-3TB',
+    product: 'Singles',
+    link: 'https://drive.google.com/file/d/19IDVft8NkUlcyH_4aBuzyw7knIPiNUCk/view?usp=drive_open',
+  },
+  {
+    id: 2,
+    series: 'DRM-6TB',
+    product: 'Doubles',
+    link: 'https://drive.google.com/file/d/15T07GwsetHvKho1qiO5rgqnl5gcSkNNj/view?usp=drive_open',
+  },
+  {
+    id: 3,
+    series: 'DRM-9TB',
+    product: 'Triples',
+    link: 'https://drive.google.com/file/d/1zUukaAFgnRWbtPOgjrQu2uZSTCzfaCd5/view',
+  },
+  {
+    id: 4,
+    series: 'DRM-12TB',
+    product: 'Quads',
+    link: 'https://drive.google.com/file/d/11aXIEFYJWFKZNdIs5kx3w5758XP3P5gi/view',
+  },
+  {
+    id: 5,
+    series: 'DSM-3TB',
+    product: 'Singles',
+    link: 'https://drive.google.com/file/d/1-vEfeHbt_ksvm4OSp7Ftyx0-lLJKDVpB/view',
+  },
+  {
+    id: 6,
+    series: 'DSM-6TB',
+    product: 'Doubles',
+    link: 'https://drive.google.com/file/d/1PB4qxiCv7357KI0G9wZfwJ8k8ITi-l3o/view',
+  },
+  {
+    id: 7,
+    series: 'DSM-9TB',
+    product: 'Triples',
+    link: 'https://drive.google.com/file/d/1ejBHBeG6wgcw0wTE6kRICV_yHSL4rqcz/view',
+  },
+  {
+    id: 8,
+    series: 'DSM-12TB',
+    product: 'Quads',
+    link: 'https://drive.google.com/file/d/18QWI-MZy-dJuxgoA-3LxGL-vMzhMOuEg/view',
+  },
+];
+
+export { libraries, tnb };
