412a5edb07f78633a1617ef85241b4a8d1dec2a7
onionie <gutierrez.onionie@gmail.com>
2022-09-21 22:58:58 -0700

    InstallationInstruction: Video and Video Description

--- src/pages/ArchitectPage/InstallationInstructions.js
+++ src/pages/ArchitectPage/InstallationInstructions.js
@@ -4,7 +4,38 @@ import styles from './InstallationInstructions.module.css';
 const InstallationInstructions = () => {
   return (
     <section className={styles.instruction_section}>
-      InstallationInstructions
+      <div className={styles.instruction_title}>Installation Instructions</div>
+
+      <div className={styles.instruction_video_box}>
+        <div className={styles.instruction_video_title}>
+          D-SERIES TABLES & BENCHES / HANDLING, CARE AND OPERATION
+        </div>
+        <div className={styles.instruction_video_grid}>
+          <div>
+            <div className={styles.iframe_container}>
+              <iframe
+                width="560"
+                height="315"
+                src="https://www.youtube.com/embed/j_y-2ZXXN8M"
+                title="YouTube video player"
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              ></iframe>
+            </div>
+          </div>
+          <div className={styles.instruction_video_body}>
+            <div className={styles.instruction_video_body_title}>
+              Standard procedures and guidelines instructional video
+            </div>
+            <div className={styles.instruction_video_body_desc}>
+              Special features of our tables & benches, locking mechanisms and
+              some additional valuable tips
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.instruction_list_box}></div>
     </section>
   );
 };
