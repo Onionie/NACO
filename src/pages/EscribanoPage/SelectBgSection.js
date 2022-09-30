a59dbd506b02e6701a585fd9de339cca5f0cc9e6
onionie <gutierrez.onionie@gmail.com>
2022-09-30 03:50:21 -0700

    Escribano: Select Background Body Descrption

--- src/pages/EscribanoPage/SelectBgSection.js
+++ src/pages/EscribanoPage/SelectBgSection.js
@@ -20,11 +20,14 @@ const SelectBgSection = () => {
         ))}
       </Carousel>
       <div>
-        <div className={styles.select_bg_title}>Meet The Butterfly Menu</div>
+        <div className={styles.select_bg_title}>
+          Pick A Background for Every Subject!
+        </div>
         <div className={styles.select_bg_description}>
-          Our butterfly menu gives you access to all the tools you need to use
-          Escribano and our displayed color pallet makes it easy to select the
-          color pen you need.
+          Escribano is an interactive whiteboard and so much more. You can
+          select from our 14 different backgrounds to start your lesson.
+          Escribano has a background selection for every class like math, music
+          and even sports!
         </div>
       </div>
     </div>
