46be796c4d043746e5fdc73225fe7e01b9331b7d
onionie <gutierrez.onionie@gmail.com>
2022-09-30 04:01:03 -0700

    Escribano: Virtual Keyboard Title and Description

--- src/pages/EscribanoPage/SelectBgSection.js
+++ src/pages/EscribanoPage/SelectBgSection.js
@@ -6,29 +6,46 @@ import { background } from './MapItems';
 
 const SelectBgSection = () => {
   return (
-    <div className={styles.select_bg_section}>
-      <Carousel
-        className="background_carousel"
-        indicators={true}
-        controls={true}
-        variant="light"
-      >
-        {background.map((bg) => (
-          <Carousel.Item key={bg.id}>
-            <img className="d-block w-100" src={bg.image} alt={bg.alt} />
-          </Carousel.Item>
-        ))}
-      </Carousel>
-      <div>
-        <div className={styles.select_bg_title}>
-          Pick A Background for Every Subject!
+    <div>
+      {/* Background */}
+      <div className={styles.select_bg_section}>
+        <Carousel
+          className="background_carousel"
+          indicators={true}
+          controls={true}
+          variant="light"
+        >
+          {background.map((bg) => (
+            <Carousel.Item key={bg.id}>
+              <img className="d-block w-100" src={bg.image} alt={bg.alt} />
+            </Carousel.Item>
+          ))}
+        </Carousel>
+        <div>
+          <div className={styles.select_bg_title}>
+            Pick A Background for Every Subject!
+          </div>
+          <div className={styles.select_bg_description}>
+            Escribano is an interactive whiteboard and so much more. You can
+            select from our 14 different backgrounds to start your lesson.
+            Escribano has a background selection for every class like math,
+            music and even sports!
+          </div>
         </div>
-        <div className={styles.select_bg_description}>
-          Escribano is an interactive whiteboard and so much more. You can
-          select from our 14 different backgrounds to start your lesson.
-          Escribano has a background selection for every class like math, music
-          and even sports!
+      </div>
+
+      {/*  Virtual Keyboard */}
+      <div className={styles.virtual_keyboard_section}>
+        <div>
+          <div className={styles.virtual_keyboard_title}>Virtual Keyboard</div>
+          <div className={styles.virtual_keyboard_description}>
+            Escribano is an interactive whiteboard and so much more. You can
+            select from our 14 different backgrounds to start your lesson.
+            Escribano has a background selection for every class like math,
+            music and even sports!
+          </div>
         </div>
+        <div></div>
       </div>
     </div>
   );
