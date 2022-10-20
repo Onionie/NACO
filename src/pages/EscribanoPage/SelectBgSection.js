993c2f96b476f364eec34782a8d7f23393075493
onionie <gutierrez.onionie@gmail.com>
2022-10-19 23:09:41 -0700

    Escribano: Dictation Mode - text animation optimized

--- src/pages/EscribanoPage/SelectBgSection.js
+++ src/pages/EscribanoPage/SelectBgSection.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import styles from './SelectBgSection.module.css';
 
@@ -7,7 +7,32 @@ import { background } from './MapItems';
 
 import virtualKeyboard from '../../assets/images/Escribano/esc_keyboard_screen2.png';
 
+function useOnScreen(options) {
+  const [ref, setRef] = useState(null);
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => {
+      setVisible(entry.isIntersecting);
+    }, options);
+
+    if (ref) {
+      observer.observe(ref);
+    }
+
+    return () => {
+      if (ref) {
+        observer.unobserve(ref);
+      }
+    };
+  }, [ref, options]);
+
+  return [setRef, visible];
+}
+
 const SelectBgSection = () => {
+  const [setRef, visible] = useOnScreen({ rootMargin: '-10px' });
+
   return (
     <div className={styles.keyboard_section}>
       {/* Background */}
@@ -54,28 +79,46 @@ const SelectBgSection = () => {
           </div>
         </div>
         <div
+          ref={setRef}
           className={styles.bg_image_div}
           data-aos="fade-left"
           data-aos-delay="1500"
         >
           <img src={virtualKeyboard} alt="virtual_keyboard_screen" />
-          <div className={styles.dict_text_box}>
-            <div className={`${styles.dict_text} ${styles.dict_text_1}`}>
-              Tell
+
+          {visible ? (
+            <div className={styles.dict_text_box}>
+              <div className={`${styles.dict_text} ${styles.dict_text_1}`}>
+                Tell
+              </div>
+              <div className={`${styles.dict_text} ${styles.dict_text_2}`}>
+                Escribano
+              </div>
+              <div className={`${styles.dict_text} ${styles.dict_text_3}`}>
+                what
+              </div>
+              <div className={`${styles.dict_text} ${styles.dict_text_4}`}>
+                to
+              </div>
+              <div className={`${styles.dict_text} ${styles.dict_text_5}`}>
+                write.
+              </div>
+              <div className={`${styles.dict_text} ${styles.dict_text_6}`}>
+                It's
+              </div>
+              <div className={`${styles.dict_text} ${styles.dict_text_7}`}>
+                an
+              </div>
+              <div className={`${styles.dict_text} ${styles.dict_text_8}`}>
+                amazing
+              </div>
+              <div className={`${styles.dict_text} ${styles.dict_text_9}`}>
+                feature!
+              </div>
             </div>
-            <div className={`${styles.dict_text} ${styles.dict_text_2}`}>
-              Escribano
-            </div>
-            <div className={`${styles.dict_text} ${styles.dict_text_3}`}>
-              what
-            </div>
-            <div className={`${styles.dict_text} ${styles.dict_text_4}`}>
-              to
-            </div>
-            <div className={`${styles.dict_text} ${styles.dict_text_5}`}>
-              write.
-            </div>
-          </div>
+          ) : (
+            <></>
+          )}
         </div>
       </div>
     </div>
