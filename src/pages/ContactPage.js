a48ed4b76d101812e4bbcf072428f5d02d816a62
onionie <gutierrez.onionie@gmail.com>
2022-10-21 23:48:42 -0700

    ContactUs: Outline

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -4,7 +4,25 @@ import styles from './ContactPage.module.css';
 const ContactPage = () => {
   return (
     <section className={styles.contact_page}>
-      <div className={styles.contact_title}> Contact Us</div>
+      {/* <div className={styles.contact_title}> Contact Us </div> */}
+      <div>
+        <div className={styles.contact_sect_title}>Project Management Team</div>
+        <div className={styles.contact_cards}>
+          <div className={styles.contact_card}>
+            <div className={styles.cards_name}>Sayra Mojica</div>
+            <div className={styles.cards_title}>
+              director of project management
+            </div>
+            <a
+              href="mailto:sayra@nelsonadamsnaco.com"
+              className={styles.cards_email}
+            >
+              Sayra@nelsonadamsnaco.com
+            </a>
+            <div className={styles.cards_extension}> Ext. 105</div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
