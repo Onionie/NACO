cc015e6f32d20f4b0d5eda2ea02a331e7e310bbc
onionie <gutierrez.onionie@gmail.com>
2022-10-23 23:44:37 -0700

    ContactUs: Icons

--- src/components/Contacts/ContactCard.js
+++ src/components/Contacts/ContactCard.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import styles from './ContactCard.module.css';
+import { MdEmail } from 'react-icons/md';
+import { BsFillTelephoneFill } from 'react-icons/bs';
 
 const ContactCard = (props) => {
   return (
     <div className={styles.contact_card}>
       <div className={styles.cards_name}>{props.name}</div>
       <div className={styles.cards_title}>{props.title}</div>
-      <a href={`mailto:${props.email}`} className={styles.cards_email}>
-        {props.email}
-      </a>
-      <div className={styles.cards_extension}>{props.extNumber}</div>
+      <div className={styles.cards_email_box}>
+        <MdEmail />
+        <a href={`mailto:${props.email}`} className={styles.cards_email}>
+          {props.email}
+        </a>
+      </div>
+      {props.extNumber ? (
+        <div className={styles.cards_extension}>
+          <BsFillTelephoneFill />
+          {props.extNumber}
+        </div>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
