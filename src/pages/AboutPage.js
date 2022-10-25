f3a2f1779645613a4473693aa819f467078672ac
onionie <gutierrez.onionie@gmail.com>
2022-10-25 02:38:49 -0700

    AboutUs: Mission Body

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -10,6 +10,22 @@ const AboutPage = () => {
         <div className={styles.mission_image}>
           <img src={missionImg} alt="collage_image" />
         </div>
+        <div className={styles.mission_body}>
+          <div className={styles.mission_header}>
+            At Nelson Adams NACO we have taken our products to the next level
+            making them more{' '}
+            <span className={styles.environment_tag}>
+              environmentally friendly
+            </span>
+            , functional, easy, extraordinary, and the best way to optimize your
+            space.
+          </div>
+          <div className={styles.mission_subheader}>
+            We take pride in our work and we continue a tradition that started
+            in 1953. Since then we reinvented our processes, quality and the day
+            to day operation but culture and family values stayed the same.
+          </div>
+        </div>
       </div>
     </section>
   );
