d36a8ce6bf03e9f3cf24735557bf11e74a943374
onionie <gutierrez.onionie@gmail.com>
2022-11-22 21:15:44 -0800

    Slider: Clean up

--- src/components/ProductsPage/SliderContent.js
+++ src/components/ProductsPage/SliderContent.js
@@ -6,8 +6,6 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import './Slider.css';
 
-// import chalkboard from '../../assets/images/VisualBoards/chalkboard.jpg';
-
 const SliderContent = (props) => {
   const [showModal, setShowModal] = useState(false);
 
@@ -55,30 +53,6 @@ const SliderContent = (props) => {
     <section className="slider">
       <div className="slider_title">{props.header}</div>
       <div className="item">
-        {/* <div className={showModal ? `overlay` : `hidden`}>
-          <div className="modalz">
-            <div className="modal_box">
-              <div className="image_box">
-                <img src={chalkboard} alt="chalkboard" />
-              </div>
-
-              <div className="description_box">
-                <div className="description_title">Chalkboard</div>
-                <div className="description_paragraph">
-                  Chalkboards substrate construction consist of e3 porcelain on
-                  1/2" particleboard with optional .005 or .015 moisture barrier
-                  backer, with standard satin clear anodized aluminum that
-                  include 2ea: C-4 side trim, standard CR-4 blade tray with
-                  radius edges.
-                </div>
-                <div className="action_box">
-                  <div onClick={showModalHandler}>OK</div>
-                  <div>ARCHITECT CORNER</div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div> */}
         <Slider {...settings}>
           {props.products.map((prod, idx) => {
             return (
