c43cca1ee75bdecae43fd86c4638e6beaf0e12d0
onionie <gutierrez.onionie@gmail.com>
2022-11-29 20:53:49 -0800

    DisplayCases: Animations Added

--- src/components/SpecificProduct/DisplayCases/DetailsAndLight.js
+++ src/components/SpecificProduct/DisplayCases/DetailsAndLight.js
@@ -6,11 +6,11 @@ import hdc_led from '../../../assets/images/DisplayCases/HingedDisplayCases/hdc_
 const DetailsAndLight = (props) => {
   return (
     <div className={styles.dc_detail_box}>
-      <div>
+      <div data-aos="fade-up" data-aos-delay="500">
         <div className={styles.dc_detail_title}>{props.dc_title}</div>
         <img src={props.dc_image} alt="display_case_details" />
       </div>
-      <div>
+      <div data-aos="fade-up" data-aos-delay="800">
         <div className={styles.dc_detail_title}>Available LED Lights</div>
         <img src={hdc_led} alt="display_case_led_lights" />
       </div>
