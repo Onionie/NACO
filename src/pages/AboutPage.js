0b11f66fcd3df456d24556fd8188ded082a9ef1e
onionie <gutierrez.onionie@gmail.com>
2022-12-07 20:58:52 -0800

    AboutUs: Move Project section up

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -23,6 +23,103 @@ const AboutPage = () => {
 
   return (
     <section className={styles.about_section}>
+      {/* PROJECTS Section */}
+      <div className={styles.projects_body}>
+        <div
+          className={styles.projects_title}
+          data-aos="fade-down"
+          data-aos-delay="1800"
+        >
+          Projects
+        </div>
+        <div
+          className={styles.projects_gallery}
+          data-aos="fade-up"
+          data-aos-delay="500"
+        >
+          <div className={styles.projects_gallery_item}>
+            <div className={styles.gallery_image}>
+              <img src={lydiksen} alt="lydiksen_school" />
+            </div>
+            <div className={styles.item_title}>Lydiksen Elementary School</div>
+            <div className={styles.item_location}>
+              <IoLocationSharp /> PLEASANTON, CA
+            </div>
+          </div>
+          <div className={styles.projects_gallery_item}>
+            <div className={styles.gallery_image}>
+              <img src={tokay} alt="tokay_school" />
+            </div>
+            <div className={styles.item_title}>Tokay High School</div>
+            <div className={styles.item_location}>
+              <IoLocationSharp /> LODI, CA
+            </div>
+          </div>
+          <div className={styles.projects_gallery_item}>
+            <div className={styles.gallery_image}>
+              <img src={hinsdale} alt="hinsdale_school" />
+            </div>
+            <div className={styles.item_title}>Hinsdale South High School</div>
+            <div className={styles.item_location}>
+              <IoLocationSharp /> AURORA, IL
+            </div>
+          </div>
+          <div className={styles.projects_gallery_item}>
+            <div className={styles.gallery_image}>
+              <img src={lomaVista} alt="lomaVista_school" />
+            </div>
+            <div className={styles.item_title}>
+              Loma Vista Elementary School
+            </div>
+            <div className={styles.item_location}>
+              <IoLocationSharp /> HERMISTON, OR
+            </div>
+          </div>
+          <div className={styles.projects_gallery_item}>
+            <div className={styles.gallery_image}>
+              <img src={thurgood} alt="thurgood_marshall_school" />
+            </div>
+            <div className={styles.item_title}>
+              Thurgood Marshall High School
+            </div>
+            <div className={styles.item_location}>
+              <IoLocationSharp /> SAN FRANCISCO, CA
+            </div>
+          </div>
+          <div className={styles.projects_gallery_item}>
+            <div className={styles.gallery_image}>
+              <img src={delOro} alt="del_oro_school" />
+            </div>
+            <div className={styles.item_title}>Del Oro High School</div>
+            <div className={styles.item_location}>
+              <IoLocationSharp />
+              BAKERSFIELD, CA
+            </div>
+          </div>
+          <div className={styles.projects_gallery_item}>
+            <div className={styles.gallery_image}>
+              <img src={rocky} alt="rocky_elementary_school" />
+            </div>
+            <div className={styles.item_title}>Rocky Elementary School</div>
+            <div className={styles.item_location}>
+              <IoLocationSharp />
+              HERMISTON, OR
+            </div>
+          </div>
+          <div className={styles.projects_gallery_item}>
+            <div className={styles.gallery_image}>
+              <img src={northBergen} alt="north_bergen_school" />
+            </div>
+            <div className={styles.item_title}>North Bergen High School</div>
+            <div className={styles.item_location}>
+              <IoLocationSharp />
+              SECAUCUS, NJ
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Mission Section */}
       <div className={styles.mission_box}>
         <div
           className={styles.mission_image}
@@ -50,98 +147,6 @@ const AboutPage = () => {
             We take pride in our work and we continue a tradition that started
             in 1953. Since then we reinvented our processes, quality and the day
             to day operation but culture and family values stayed the same.
-          </div>
-        </div>
-      </div>
-
-      {/* PROJECTS Section */}
-      <div className={styles.projects_body}>
-        <div
-          className={styles.projects_title}
-          data-aos="fade-down"
-          data-aos-delay="1800"
-        >
-          Projects
-        </div>
-      </div>
-      <div
-        className={styles.projects_gallery}
-        data-aos="fade-up"
-        data-aos-delay="500"
-      >
-        <div className={styles.projects_gallery_item}>
-          <div className={styles.gallery_image}>
-            <img src={lydiksen} alt="lydiksen_school" />
-          </div>
-          <div className={styles.item_title}>Lydiksen Elementary School</div>
-          <div className={styles.item_location}>
-            <IoLocationSharp /> PLEASANTON, CA
-          </div>
-        </div>
-        <div className={styles.projects_gallery_item}>
-          <div className={styles.gallery_image}>
-            <img src={tokay} alt="tokay_school" />
-          </div>
-          <div className={styles.item_title}>Tokay High School</div>
-          <div className={styles.item_location}>
-            <IoLocationSharp /> LODI, CA
-          </div>
-        </div>
-        <div className={styles.projects_gallery_item}>
-          <div className={styles.gallery_image}>
-            <img src={hinsdale} alt="hinsdale_school" />
-          </div>
-          <div className={styles.item_title}>Hinsdale South High School</div>
-          <div className={styles.item_location}>
-            <IoLocationSharp /> AURORA, IL
-          </div>
-        </div>
-        <div className={styles.projects_gallery_item}>
-          <div className={styles.gallery_image}>
-            <img src={lomaVista} alt="lomaVista_school" />
-          </div>
-          <div className={styles.item_title}>Loma Vista Elementary School</div>
-          <div className={styles.item_location}>
-            <IoLocationSharp /> HERMISTON, OR
-          </div>
-        </div>
-        <div className={styles.projects_gallery_item}>
-          <div className={styles.gallery_image}>
-            <img src={thurgood} alt="thurgood_marshall_school" />
-          </div>
-          <div className={styles.item_title}>Thurgood Marshall High School</div>
-          <div className={styles.item_location}>
-            <IoLocationSharp /> SAN FRANCISCO, CA
-          </div>
-        </div>
-        <div className={styles.projects_gallery_item}>
-          <div className={styles.gallery_image}>
-            <img src={delOro} alt="del_oro_school" />
-          </div>
-          <div className={styles.item_title}>Del Oro High School</div>
-          <div className={styles.item_location}>
-            <IoLocationSharp />
-            BAKERSFIELD, CA
-          </div>
-        </div>
-        <div className={styles.projects_gallery_item}>
-          <div className={styles.gallery_image}>
-            <img src={rocky} alt="rocky_elementary_school" />
-          </div>
-          <div className={styles.item_title}>Rocky Elementary School</div>
-          <div className={styles.item_location}>
-            <IoLocationSharp />
-            HERMISTON, OR
-          </div>
-        </div>
-        <div className={styles.projects_gallery_item}>
-          <div className={styles.gallery_image}>
-            <img src={northBergen} alt="north_bergen_school" />
-          </div>
-          <div className={styles.item_title}>North Bergen High School</div>
-          <div className={styles.item_location}>
-            <IoLocationSharp />
-            SECAUCUS, NJ
           </div>
         </div>
       </div>
