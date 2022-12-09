829eb2132e6d1141624f925b7d6b288ef4bdb309
onionie <gutierrez.onionie@gmail.com>
2022-12-09 03:04:56 -0800

    PatriciaPage: Hero Subheader

--- src/pages/PatriciaPage/PatriciaPage.js
+++ src/pages/PatriciaPage/PatriciaPage.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import styles from './PatriciaPage.module.css';
 
-// Images
-import heroBg from '../../assets/images/Patricia/abstract_connection.jpg';
-
 const PatriciaPage = () => {
   return (
     <div className={styles.patricia}>
