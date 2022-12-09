8a01eabc82656dbe2b6d1ecef37a5f3618a08a1b
onionie <gutierrez.onionie@gmail.com>
2022-12-09 03:24:43 -0800

    PatriciaPage: Hero Animation

--- src/pages/PatriciaPage/PatriciaPage.js
+++ src/pages/PatriciaPage/PatriciaPage.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './PatriciaPage.module.css';
 
+// Animation
+import Aos from 'aos';
+import 'aos/dist/aos.css';
+
 const PatriciaPage = () => {
+  useEffect(() => {
+    Aos.init({ duration: 2000, once: 'true' }, []);
+  });
+
   return (
     <div className={styles.patricia}>
       <div className={styles.patricia_hero}>
-        <div className={styles.patricia_header}>Patricia</div>
-        <div className={styles.patricia_subheader}>
+        <div
+          className={styles.patricia_header}
+          data-aos="fade-up"
+          data-aos-delay="300"
+        >
+          Patricia
+        </div>
+        <div
+          className={styles.patricia_subheader}
+          data-aos="fade-up"
+          data-aos-delay="1200"
+        >
           Escribano's Virtual Assistant
         </div>
       </div>
