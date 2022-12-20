bb3d54cedd90f7cf75408d4728611a0825dac4a5
onionie <gutierrez.onionie@gmail.com>
2022-12-20 02:44:17 -0800

    created list of commands1

---
+++ src/pages/PatriciaPage/commands.js
@@ -0,0 +1,41 @@
+const commands1 = [
+  { id: 1, command: 'Patricia, activate normal pen.' },
+  {
+    id: 2,
+    command: 'Patricia, I want to draw.',
+  },
+  {
+    id: 3,
+    command: 'Patricia, I need a pen.',
+  },
+  {
+    id: 4,
+    command: 'Patricia, Ictivate calligraphy pen.',
+  },
+  {
+    id: 5,
+    command: 'Patricia, Ictivate normal pen.',
+  },
+  {
+    id: 6,
+    command: 'Patricia, Ictivate marker.',
+  },
+  {
+    id: 7,
+    command: 'Patricia, I want to draw a circle.',
+  },
+  {
+    id: 8,
+    command: 'Patricia, I want to draw a square.',
+  },
+  {
+    id: 9,
+    command: 'Patricia, I want to draw a rectangle.',
+  },
+  {
+    id: 10,
+    command: 'Patricia, I want to draw a triangle.',
+  },
+];
+
+export { commands1 };
