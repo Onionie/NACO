7a290b86403a0208e3e6b7c916f590399e46a2b4
onionie <gutierrez.onionie@gmail.com>
2022-12-20 03:05:53 -0800

    More commands

--- src/pages/PatriciaPage/commands.js
+++ src/pages/PatriciaPage/commands.js
@@ -38,4 +38,30 @@ const commands1 = [
   },
 ];
 
-export { commands1 };
+const commands2 = [
+  { id: 0, command: 'Patricia, open google.' },
+  { id: 1, command: 'Patricia, I want to navigate the web.' },
+  { id: 2, command: 'Patricia, activate smart pen.' },
+  { id: 3, command: 'Patricia, I want to draw a straight line.' },
+  { id: 4, command: 'Patricia, activate notes.' },
+  { id: 5, command: 'Patricia, activate laser.' },
+  { id: 6, command: 'Patricia, start dictation.' },
+  { id: 7, command: 'Patricia, previous page.' },
+  { id: 8, command: 'Patricia, next page.' },
+  { id: 9, command: 'Patricia, navigate to page.' },
+];
+
+const commands3 = [
+  { id: 0, command: 'Patricia, open youtube.' },
+  { id: 1, command: 'Patricia, open email.' },
+  { id: 2, command: 'Patricia, send email.' },
+  { id: 3, command: 'Patricia, I want to make a call.' },
+  { id: 4, command: 'Patricia, open megameetings.' },
+  { id: 5, command: 'Patricia, I want to start a meeting.' },
+  { id: 6, command: 'Patricia, I want to make a conference.' },
+  { id: 7, command: 'Patricia, go to sleep.' },
+  { id: 8, command: 'Patricia, turn off.' },
+  { id: 9, command: 'Patricia, shut down.' },
+];
+
+export { commands1, commands2, commands3 };
